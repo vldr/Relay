914e22da63dba64d6e62b524ba8fe5cbe78f964d
Vlad <44514204+vldr@users.noreply.github.com>
2023-05-15 14:11:46 -0500

    Added create room and join room packet handling.

--- src/relay.rs
+++ src/relay.rs
@@ -1,5 +1,4 @@
 use std::net::{SocketAddr};
-use std::str::FromStr;
 use std::sync::{Arc};
 
 use uuid::{Uuid};
@@ -13,11 +12,21 @@ use dashmap::{DashMap};
 
 use serde::{Deserialize, Serialize};
 
+const DEFAULT_ROOM_SIZE: u8 = 2;
+
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
-enum Packet {
-    CreateRoom { size: Option<u64> },
+enum ReceivePacket {
+    CreateRoom { size: Option<u8> },
     JoinRoom { id: String },
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum TransmitPacket {
+    JoinRoom,
+    CreateRoom { id: String },
+    Error { message: String },
 }
 
 struct Client 
@@ -57,15 +66,19 @@ impl Client
 struct Room 
 {
     id: String,
-    size: u64,
-    users: Vec<Arc<Client>> 
+    users: Vec<Arc<Client>>,
+    size: u8,
 }
 
 impl Room 
 {
-    fn new(size: u64) -> Uuid
+    fn new(size: u8) -> Room
     {
-        return Uuid::new_v4();
+        return Room {
+            id: Uuid::new_v4().to_string(),
+            users: vec![],
+            size,
+        };
     }
 
     fn add(&mut self, client: Arc<Client>) 
@@ -87,6 +100,7 @@ impl Room
 pub struct Relay 
 {
     rooms: dashmap::DashMap<String, Room>,
+    sockets: dashmap::DashMap<SocketAddr, String>,
 }
 
 impl Relay 
@@ -95,6 +109,7 @@ impl Relay
     {
         Relay {
             rooms: DashMap::new(),
+            sockets: DashMap::new(),
         }
     }
 
@@ -105,14 +120,14 @@ impl Relay
             Ok(websocket_stream) => 
             {
                 let client = Client::new(socket_addr, websocket_stream);
-
+    
                 while let Some(message) = client.next_message().await
                 {
                     match message
                     {
                         Ok(message) => 
                         {
-                            self.handle_message(&client, message).await;
+                            self.handle_message(client.clone(), message).await;
                         },
                         Err(error) => 
                         {
@@ -130,30 +145,90 @@ impl Relay
         
     }
 
-    async fn handle_message(&self, client: &Client, message: Message)
+    async fn handle_message(&self, client: Arc<Client>, message: Message)
     {
         if message.is_text()
         {
+            if let Some(socket) = self.sockets.get(&client.socket_addr) 
+            {
+                if let Some(room) = self.rooms.get(socket.value()) 
+                {
+                    client.send_message(Message::Text(room.id.clone())).await;
+                    return;
+                }
+            }
+
             if let Ok(text) = message.into_text()
             {
                 if let Ok(packet) = serde_json::from_str(&text) 
                 {
                     match packet {
-                        Packet::CreateRoom { size } => self.handle_create_room(client, size),
-                        Packet::JoinRoom { id } => self.handle_join_room(client, id),
+                        ReceivePacket::CreateRoom { size } => self.handle_create_room(client, size).await,
+                        ReceivePacket::JoinRoom { id } => self.handle_join_room(client, id).await,
                     }
                 }
             }
         }
     }
 
-    fn handle_create_room(&self, client: &Client, size: Option<u64>)
+    async fn handle_create_room(&self, client: Arc<Client>, size_option: Option<u8>)
     {
-        println!("CreateRoom {:?}", size);
+        let size = size_option.unwrap_or(DEFAULT_ROOM_SIZE);
+        if size == 0 
+        {
+            return self.send_error_packet(client, format!("The size value of '{}' is not valid.", size)).await;   
+        }
+
+        let mut room = Room::new(size);
+        room.add(client.clone());
+
+        let packet = TransmitPacket::CreateRoom { id: room.id.clone() };
+
+        self.sockets.insert(client.socket_addr, room.id.clone());
+        self.rooms.insert(room.id.clone(), room);
+
+        self.send_packet(client, packet).await;
     }
 
-    fn handle_join_room(&self, client: &Client, id: String)
+    async fn handle_join_room(&self, client: Arc<Client>, id: String)
     {
-        println!("Join Room {:?}", id);
+        if let Some(mut room) = self.rooms.get_mut(&id) 
+        {
+            if room.length() >= room.size.into()
+            {
+                return self.send_error_packet(client, format!("The room is full.")).await;
+            }
+
+            self.sockets.insert(client.socket_addr, room.id.clone());
+            room.add(client.clone());
+
+            self.send_packet(client, TransmitPacket::JoinRoom).await;
+        }
+        else 
+        {
+            return self.send_error_packet(client, format!("The room '{}' does not exist.", id)).await;  
+        }
     }
+
+    async fn send_packet(&self, client: Arc<Client>, packet: TransmitPacket)
+    {
+        let serialized_packet = serde_json::to_string(&packet).unwrap();
+
+        if let Err(error) = client.send_message(Message::Text(serialized_packet)).await 
+        {
+            println!("Failed to send packet: {}", error);
+        } 
+    }
+
+    async fn send_error_packet(&self, client: Arc<Client>, message: String)
+    {
+        let error_packet = TransmitPacket::Error { message };
+        let serialized_error_packet = serde_json::to_string(&error_packet).unwrap();
+
+        if let Err(error) = client.send_message(Message::Text(serialized_error_packet)).await 
+        {
+            println!("Failed to send error packet: {}", error);
+        }
+    }
+
 }
