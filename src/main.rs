3d8e83fbc582d1d7c82232f2b79651f070c45563
Vlad <44514204+vldr@users.noreply.github.com>
2023-06-16 21:54:16 -0500

    Add error checking.

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,19 @@ async fn main()
     let address = env::args().nth(1).unwrap_or("0.0.0.0".to_string());
     let port = env::args().nth(2).unwrap_or("1234".to_string());
 
-    let listener = TcpListener::bind(&format!("{}:{}", address, port)).await
-        .expect("Failed to bind");
-
-    let server = relay::Server::new();
-
-    println!("Listening on: {}:{}", address, port);
-
-    while let Ok((tcp_stream, _)) = listener.accept().await 
+    if let Ok(listener) = TcpListener::bind(&format!("{}:{}", address, port)).await 
     {
-        tokio::spawn( relay::Server::handle_connection(server.clone(), tcp_stream));
+        println!("Listening on: {}:{}", address, port);
+        
+        let server = relay::Server::new();
+
+        while let Ok((tcp_stream, _)) = listener.accept().await 
+        {
+            tokio::spawn( relay::Server::handle_connection(server.clone(), tcp_stream));
+        }
+    }
+    else 
+    {
+        println!("Failed to listen on: {}:{}", address, port);
     }
 }
